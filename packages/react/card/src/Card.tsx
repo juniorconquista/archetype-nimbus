58b74902ff016e4d334d070589888f8f0c94643e
Júnior Conquista <conquista.engcomp@gmail.com>
2021-10-04 11:25:59 -0300

    wip

--- packages/react/card/src/Card.tsx
+++ packages/react/card/src/Card.tsx
@@ -1,14 +1,16 @@
 //@ts-ignore
+
+
 import * as React from 'react';
 
 import { Header, Body, Footer } from './components';
 import { ICard, ICardComponents } from './card.types';
 
-const Card: React.FC<ICard> & ICardComponents = ({ children}: ICard) => (
-    <div className="card__container">
-      {children}
-    </div>
-  );
+const Card: React.FC<ICard> & ICardComponents = ({ children }: ICard) => (
+  <div className="card__container">
+    {children}
+  </div>
+);
 
 
 Card.Header = Header;
